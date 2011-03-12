303832fd33eab163fdbf169b744ddb1f747a423d
John Marshall <johnm555@gmail.com>
2011-03-11 19:54:09 -0500

    Started on the website

--- server.js
+++ server.js
@@ -1,92 +1,52 @@
-var events = require('events');
-var sys = require('sys');
-var http = require('http');
-var fs = require('fs');
+var sys = require("sys"),
+    http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs");
 
-var TestApp = function() {
-  var _self = this;
-  var counter = 0;
-  
-  var routes = {
-    '/' : function(request, response) {
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      var string = _self.indexTemplate;
-      response.write(string);
-      response.end();
-    },
-
-    '/jsupdate' : function(request, response) {
-      response.writeHead(200, {'Content-Type': 'text/javascript'});
-      updateLoop.call(this, request, response, counter);
-    },
-
-    '/jsstatus' : function(request, response) {
-      response.writeHead(200, {'Content-Type': 'text/javascript'});
-      response.write('counter: ' + counter + '\n');
-      response.end();
-    },
-
-    '/increment' : function(request, response) {
-      response.writeHead(200, {'Content-Type': 'text/plain'});
-      counter++;
-      response.write('counter: ' + counter + '\n');
-      response.end();
-    },
-
-    '/reset' : function(request, response) {
-      response.writeHead(200, {'Content-Type': 'text/plain'});
-      counter = 0;
-      response.write('reset\n');
-      response.end();
-    }
-  }
-
-  var updateLoop = function(request, response, current_counter) {
-    if(current_counter != counter) {
-      response.write('counter: ' + counter + '\n');
-      response.end();
-      return false;
-    }
+http.createServer(function(request, response) {
+    var urlstub = request.url;
+    if (urlstub=="/")
+        urlstub = "index.html";
     
-    setTimeout(function() {
-      updateLoop.call(this, request, response, current_counter);
-    }, 1000);
-  };
-
-  var _requestHandler = function(request, response) {
-    sys.puts('request: \'' + request.url + '\'');
+    console.log("urlstub: "+urlstub);
+    var uri = url.parse(urlstub).pathname;
+    console.log("uri: "+uri);
     
-    if(routes[request.url] === undefined) {
-      response.writeHead(404, {'Content-Type': 'text/plain'});
-      response.write('not found\n');
-      response.end();
-    } else {
-      routes[request.url].call(this, request, response);
-    }
-  };
-  
-  var _updateHandler = function(request, socket, head) {
-    sys.puts('update');
-  };
+    
+    var filename = path.join(process.cwd(), uri);
 
-  var _closeHandler = function() {
-    sys.puts('close');
-  };
-  
-  sys.puts('New TestApp');
-  
-  fs.readFile('./index.html', function (err, data) {
-    if (err) throw err;
-    _self.indexTemplate = data;
-    sys.puts('Template loaded');
-  });
-  
-  var _server = http.createServer().
-          addListener('request', _requestHandler)
-          .addListener('close', _closeHandler)
-          .addListener('update', _updateHandler)
-          .listen(8000);
-  sys.puts('Listening to port 8000');
-};
+    path.exists(filename, function(exists) {
+    	if(!exists) {
+    	    console.log("doesn't exist");
+    		response.writeHead(404, {"Content-Type": "text/plain"});
+    		response.write("404 Not Found");
+    		response.end();
+    		return;
+    	}
 
-new TestApp();
+
+    	fs.readFile(filename, "binary", function(err, file) {
+      	    console.log("file exists");
+    		if(err) {
+    		    console.log("err sending file");
+    		    var contenttype = "text/plain";
+    		    if (filename.match(/\.js$/)){
+    		      contenttype = "text/javascript"; 
+    		    }
+    			response.writeHead(500, {"Content-Type": contenttype});
+    			response.write("filename: "+filename+"\n");
+    			response.write(err + "\n");
+    			response.end();
+    			return;
+    		}
+            console.log("sent file: ");
+            console.log(file);
+    		response.writeHead(200);
+    		response.write(file, "binary");
+    		response.end();
+    	});
+    });
+}).listen(8080);
+
+sys.puts("Server running at http://localhost:8080/");
