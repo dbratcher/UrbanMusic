296000a4ad00bc8e121a7c9c6ac55e5a7373b059
John Marshall <johnm555@gmail.com>
2011-03-11 20:45:41 -0500

    I forget what I changed

--- frontend.js
+++ frontend.js
@@ -1,31 +1,68 @@
-
-function addToList(data,list){
-    console.log("add to list");
-    for (var i=0;i<data.length;i++){
-        list.append("<li>"+data[i]+"</li>");   
+var urbanmusic = {
+    addToList: function addToList(data,list){
+       console.log("add to list");
+       for (var i=0;i<data.length;i++){
+           list.append("<li>"+data[i]+"</li>");   
+       }
+       list.val(data[0]);
+    },
+    doload: function(){
+        console.log("doload");
+        $.ajax({
+          url: "years.json",
+          dataType: "json",
+          success: function(data){
+            var list = $("#year-list");
+            urbanmusic.addToList(data,list);
+          }
+        });
+        
+        $.ajax({
+          url: "genre.json",
+          dataType: "json",
+          success: function(data){
+            var list = $("#genre-list");
+            urbanmusic.addToList(data,list);
+          }
+        });
+        
+        $(".listen-button").bind("touch click",
+            urbanmusic.listen);
+        $(".listen-button").bind("touch click",
+            urbanmusic.randomize);
+    },
+    /*
+        set random arbitrary values 
+    */
+    randomize: function(){
+          
+    },
+    /*
+        send selected values
+        load new page
+    */
+    listen: function(){
+        var data = {};
+        data.genre = $("#genre-list").val();
+        data.years = $("#years-list").val();
+        
+        $.ajax({
+          url: "listen",
+          data: data,
+          dataType: "json",
+          success: function(data){
+            
+          },
+          error: function(){
+            
+          }
+        }); 
     }
 }
 
-function doload(){
-    console.log("doload");
-    $.ajax({
-      url: "years.json",
-      dataType: "json",
-      success: function(data){
-        var list = $("#year-list");
-        addToList(data,list);
-      }
-    });
-    
-    $.ajax({
-      url: "genre.json",
-      dataType: "json",
-      success: function(data){
-        var list = $("#genre-list");
-        addToList(data,list);
-      }
-    });
-}
+
+
+
 
 console.log("frontend.js");
-window.addEventListener("load",doload,false);
+window.addEventListener("load",urbanmusic.doload,false);
