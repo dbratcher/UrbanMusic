9f6573119b7eb31946fed1339143dfdfd0775e0c
John Marshall <johnm555@gmail.com>
2011-03-12 04:50:28 -0500

    idk

--- server.js
+++ server.js
@@ -23,44 +23,46 @@ http.createServer(function(request, response) {
     var uri = url.parse(urlstub).pathname;
     console.log("uri: "+uri);
     if(uri=="/listen"){
-	var query=qs.parse(urlstub.substring(uri.length+1));
-	console.log("Handling listen stuff");
-	var data = {};
-	data.method = "tag.gettoptracks"
-	data.tag = query.genre;
-	data.api_key="d68be9970d20265eaad5ef4c92b21fcc";
-	var post_data=qs.stringify(data);
-	var req=new xhr.XMLHttpRequest();
-	req.open("GET","http://ws.audioscrobbler.com/2.0/?"+post_data);
-	console.log(post_data);
-	req.setRequestHeader('Content-Type', "application/x-www-form-urlencoded");
-	console.log("here");
-	req.onreadystatechange = function(ev){
-		try{
-			if (req.readyState==4 && req.status == 200){	
-				var xml = req.responseText;
-				var xml_doc=libxml.parseXmlString(xml);
-				var track=xml_doc.root().childNodes()[1].childNodes()[1].childNodes()[1].text();
-				var artist=xml_doc.root().childNodes()[1].childNodes()[1].childNodes()[11].childNodes()[1].text();
-				var img=xml_doc.root().childNodes()[1].childNodes()[1].childNodes()[19].text();
-				console.log(track+" by "+artist+" with image:"+img);
-				var data={};
-				data.track=track;
-				data.artist=artist;
-				data.img=img;
-				response.writeHead(200,{"Content-Type":"application/json"});
-				response.write(JSON.stringify(data));
-				response.end();
-				console.log("returning");
-				return;
-			}
-		}
-		catch(e)
-		{
-			console.log(e);
-		}
-	}
-	req.send(post_data);
+      	var query=qs.parse(urlstub.substring(uri.length+1));
+      	console.log("Handling listen stuff");
+      	var data = {};
+      	data.method = "tag.gettoptracks"
+      	data.tag = query.genre;
+      	data.api_key="d68be9970d20265eaad5ef4c92b21fcc";
+      	var post_data=qs.stringify(data);
+      	var req=new xhr.XMLHttpRequest();
+      	req.open("GET","http://ws.audioscrobbler.com/2.0/?"+post_data);
+      	console.log(post_data);
+      	req.setRequestHeader('Content-Type', "application/x-www-form-urlencoded");
+      	console.log("here");
+      	req.onreadystatechange = function(ev){
+      		try{
+      			if (req.readyState==4 && req.status == 200){	
+      				var xml = req.responseText;
+      				var xml_doc=libxml.parseXmlString(xml);
+      				var track=xml_doc.root().childNodes()[1].childNodes()[1].childNodes()[1].text();
+      				var artist=xml_doc.root().childNodes()[1].childNodes()[1].
+      				      childNodes()[11].childNodes()[1].text();
+      				var img=xml_doc.root().childNodes()[1].childNodes()[1].childNodes()[19].text();
+      				console.log(track+" by "+artist+" with image:"+img);
+      				var data={};
+      				data.track=track;
+      				data.artist=artist;
+      				data.img=img;
+      				
+      				response.writeHead(200,{"Content-Type":"application/json"});
+      				response.write(JSON.stringify(data));
+      				response.end();
+      				console.log("returning");
+      				return;
+      			}
+      		}
+      		catch(e)
+      		{
+      			console.log(e);
+      		}
+      	}
+      	req.send(post_data);
 
     }
     else{
